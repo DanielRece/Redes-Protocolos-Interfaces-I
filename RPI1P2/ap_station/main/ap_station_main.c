b83b8834d56c8f0cd48ac69bb86846cc876f486f
Fabrialc123 <64922167+Fabrialc123@users.noreply.github.com>
2023-10-01 22:59:17 +0200

    Update ap_station_main.c

--- RPI1P2/ap_station/main/ap_station_main.c
+++ RPI1P2/ap_station/main/ap_station_main.c
@@ -141,12 +141,9 @@ void wifi_init_ap_sta(void)
     
     s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_ap();
     esp_netif_create_default_wifi_sta();
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    
-
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
